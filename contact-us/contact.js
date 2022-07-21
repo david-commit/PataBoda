ec5bb6c502a826f356584e716699f9c2bd52fb4f
Ezra-Kipchumba <ezra.kipchumba@student.moringaschool.com>
2022-07-21 11:06:17 +0300

    FAQs page alignment corrections

---
+++ contact-us/contact.js
@@ -0,0 +1,15 @@
+// const send = () => {
+//     buttons.alert ("Thank You, we are reviewing your message");
+// }
+
+// const buttons = document.getElementById("btn");
+// buttons.addEventListener("click", send);
+
+//my
+
+function theSend(){
+    const person = document.getElementById("fullname").value;
+
+    alert ("Thank You,"+ person +" we are reviewing your message");
+
+}
